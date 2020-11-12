01e31254adde33a74f60bad76fd3b7b010367ba5
250kgBeast <59151069+250kgBeast@users.noreply.github.com>
2020-11-12 12:59:31 +0200

    added delete cbv, and create, update and delete operations to staff user

--- blog_engine/urls.py
+++ blog_engine/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.PostsListView.as_view(), name='posts_list'),
     path('tags/', views.TagsListView.as_view(), name='tags_list'),
     path('post/create/', views.PostCreateView.as_view(), name='post_create'),
-    path('post/update/<str:slug>/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/edit/<str:slug>/', views.PostUpdateView.as_view(), name='post_update'),
     path('post/<str:slug>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/<str:slug>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
     path('tag/<str:slug>/', views.TagDetailView.as_view(), name='tag_detail')
 ]
--- blog_engine/views.py
+++ blog_engine/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 
 from .models import Post, Tag
@@ -6,9 +7,14 @@ from .forms import PostCreateForm
 
 
 class PostsListView(ListView):
+    """
+    List of all posts
+    """
     model = Post
     template_name = 'blog_engine/posts_list.html'
     context_object_name = 'posts'
+    paginate_by = 2
+    ordering = ['-date_pub']
 
 
 class PostDetailView(DetailView):
@@ -17,12 +23,18 @@ class PostDetailView(DetailView):
 
 
 class TagsListView(ListView):
+    """
+    List of all tags
+    """
     model = Tag
     template_name = 'blog_engine/tags_list.html'
     context_object_name = 'tags'
 
 
 class TagDetailView(DetailView):
+    """
+    List of posts with specific tag
+    """
     model = Tag
     template_name = 'blog_engine/tag_detail.html'
 
@@ -43,3 +55,14 @@ class PostUpdateView(UpdateView):
 
     def get_success_url(self):
         return reverse('post_detail', args=[self.object.slug])
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'blog_engine/post_delete.html'
+
+    def get_success_url(self):
+        return reverse('posts_list')
+
+    def get_login_url(self):
+        return reverse('posts_list')
